d19c82e036b54be03b123ac4395623cd715aeddf
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-09 21:54:06 +0530

    [lishitha]: added address book dto for request body

---
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.addressbook.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressBookDTO {
+
+    @NotBlank(message = "First name cannot be empty")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be empty")
+    private String lastName;
+
+    @NotBlank(message = "City cannot be empty")
+    private String city;
+
+    @NotBlank(message = "State cannot be empty")
+    private String state;
+
+    @NotBlank(message = "Phone number cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
+    private String phoneNumber;
+}
