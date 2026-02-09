20b13b5debe281ec48a454dd37d6bf7d2d774697
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-09 21:55:27 +0530

    [lishitha]: created custom exception for invalid id

---
+++ src/main/java/com/bridgelabz/addressbook/exception/IdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbook.exception;
+
+public class IdNotFoundException extends RuntimeException {
+
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
